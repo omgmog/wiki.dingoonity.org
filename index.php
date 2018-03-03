e8cb4c7a1010f7e0d2ae356d819c1bf3f95390ba
Max Glenister <moglenstar@gmail.com>
2018-03-03 10:09:43 +0000

    Switched to using Twig to render the pages

--- index.php
+++ index.php
@@ -1,18 +1,40 @@
 <?php
   // Basic but let's see...
+  require_once 'lib/Twig/Autoloader.php';
+  Twig_Autoloader::register();
 
   $_file = $_GET['title'];
   if (empty($_file)) {
     $_file = 'Main_Page';
   }
-  $file = 'pages/' . $_file . '.htm';
-  if (file_exists($file)) {
+  $rootPath = 'app/';
+  $file = 'pages/' . $_file . '.twig';
+  $model = array();
 
-    include($file);
-    echo '<link rel="stylesheet" href="mediawiki.css" />';
-    echo '<link rel="stylesheet" href="style.css" />';
+  $loader = new Twig_Loader_Filesystem($rootPath);
+  $twig = new Twig_Environment($loader, array(
+    'cache' => 'cache'
+  ));
 
+  // Clean up the titles
+  $charsToRemove = array(':', '_');
+  $title = str_replace($charsToRemove, ' ', $_file);
+
+  if (file_exists($rootPath . $file)) {
+    $model = array(
+      'page' => array(
+        'title' => $title,
+        'base' => pathinfo(__FILE__)['basename'],
+      )
+    );
   } else {
-    echo $file . ' does not exist!';
+    $model = array(
+      'page' => array(
+        'title'=> '404',
+        'message' => $title . ' does not exist!',
+      )
+    );
+    $file = '404.twig';
   }
+  echo $twig->render($file, $model);
 ?>
